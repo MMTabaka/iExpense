8dbfb24bde7e049f4f609a19596e61fffb7c61db
MMTabaka <miloszmtabaka@gmail.com>
2022-04-07 21:29:49 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Milosz Tabaka on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
